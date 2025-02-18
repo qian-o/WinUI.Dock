94b7bd1c3eb3b5894103c6ac7268b3767d65be85
王先生 <1324771795@qq.com>
2025-02-18 11:46:07 +0800

    添加依赖属性以支持布局管理
    对 `DockingManager.xaml.cs` 文件进行了修改，新增了多个依赖属性（LeftSide、TopSide、RightSide、BottomSide 和 RootPanel），并为它们添加了相应的属性访问器（getter 和 setter）。同时，更新了导入的命名空间，以支持新的功能和特性。

--- src/Dock/DockingManager.xaml.cs
+++ src/Dock/DockingManager.xaml.cs
@@ -1,24 +1,69 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Markup;
 
 namespace Dock;
 
+[ContentProperty(Name = nameof(RootPanel))]
 public sealed partial class DockingManager : Control
 {
-    // ILayoutGroup
+    public static readonly DependencyProperty LeftSideProperty = DependencyProperty.Register(nameof(LeftSide),
+                                                                                             typeof(LayoutSide),
+                                                                                             typeof(DockingManager),
+                                                                                             new PropertyMetadata(null));
 
-    // LayoutDocument - 代表一个 Document
-    // LayoutDocumentGroup - 代表一组 LayoutDocument
+    public static readonly DependencyProperty TopSideProperty = DependencyProperty.Register(nameof(TopSide),
+                                                                                            typeof(LayoutSide),
+                                                                                            typeof(DockingManager),
+                                                                                            new PropertyMetadata(null));
 
-    // LayoutAnchorable - 代表一个 Anchorable
-    // LayoutAnchorGroup - 代表一组 LayoutAnchorable
+    public static readonly DependencyProperty RightSideProperty = DependencyProperty.Register(nameof(RightSide),
+                                                                                              typeof(LayoutSide),
+                                                                                              typeof(DockingManager),
+                                                                                              new PropertyMetadata(null));
 
-    // LayoutSide - Left, Top, Right, Bottom
+    public static readonly DependencyProperty BottomSideProperty = DependencyProperty.Register(nameof(BottomSide),
+                                                                                               typeof(LayoutSide),
+                                                                                               typeof(DockingManager),
+                                                                                               new PropertyMetadata(null));
 
-    // LayoutPanel - 代表中心区域的 Panel (Document, Anchorable)
-    // LayoutPanel.Children - ILayoutGroup[] 使用 DockPanel 布局
+    public static readonly DependencyProperty RootPanelProperty = DependencyProperty.Register(nameof(RootPanel),
+                                                                                              typeof(LayoutPanel),
+                                                                                              typeof(DockingManager),
+                                                                                              new PropertyMetadata(null));
 
     public DockingManager()
     {
         DefaultStyleKey = typeof(DockingManager);
+    }
+
+    public LayoutSide? LeftSide
+    {
+        get => (LayoutSide)GetValue(LeftSideProperty);
+        set => SetValue(LeftSideProperty, value);
+    }
+
+    public LayoutSide? TopSide
+    {
+        get => (LayoutSide)GetValue(TopSideProperty);
+        set => SetValue(TopSideProperty, value);
+    }
+
+    public LayoutSide? RightSide
+    {
+        get => (LayoutSide)GetValue(RightSideProperty);
+        set => SetValue(RightSideProperty, value);
+    }
+
+    public LayoutSide? BottomSide
+    {
+        get => (LayoutSide)GetValue(BottomSideProperty);
+        set => SetValue(BottomSideProperty, value);
+    }
+
+    public LayoutPanel? RootPanel
+    {
+        get => (LayoutPanel)GetValue(RootPanelProperty);
+        set => SetValue(RootPanelProperty, value);
     }
 }
