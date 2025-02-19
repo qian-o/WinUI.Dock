ca20a0ec7fb7ed8752a68cf7a53a21a600c59716
王先生 <1324771795@qq.com>
2025-02-19 18:23:04 +0800

    更新 NuGet 包并重构布局和转换器
    在 `Directory.Packages.props` 中添加多个 NuGet 包版本信息。
    删除 `SideToOrientationConverter`，新增 `SideToAngleConverter` 类以实现角度转换。
    在 `Dock.csproj` 中添加新包引用。
    在 `LayoutSide.xaml` 中替换转换器并更新布局为 `LayoutTransformControl`。
    在 `MainWindow.xaml` 中调整 `DockingManager` 布局，优化界面元素组织。

---
+++ src/Dock/Converters/SideToAngleConverter.cs
@@ -0,0 +1,21 @@
+﻿using Dock.Enums;
+using Microsoft.UI.Xaml.Data;
+
+namespace Dock.Converters;
+
+public sealed partial class SideToAngleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return (Side)value switch
+        {
+            Side.Left or Side.Right => 90,
+            _ => 0
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
