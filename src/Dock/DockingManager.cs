b4713abbf16b60345a4c5f5ab0345c080ed1e68b
王先生 <1324771795@qq.com>
2025-02-21 00:38:30 +0800

    添加文档和停靠管理器的依赖属性
    在 `Document.cs` 中添加了 `TitleProperty`、`CanAnchorProperty` 和 `CanCloseProperty` 依赖属性，支持 XAML 绑定和设置文档的标题、锚定状态和关闭状态。
    
    在 `DockingManager.cs` 中新增 `DockingManager` 类，包含四个方向的 `ObservableCollection<Document>` 属性和 `Children` 属性，以管理文档和布局容器，增强了 XAML 属性绑定的能力。

---
+++ src/Dock/DockingManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Markup;
+
+namespace Dock;
+
+[ContentProperty(Name = nameof(Children))]
+public partial class DockingManager : Control
+{
+    public DockingManager()
+    {
+        DefaultStyleKey = typeof(DockingManager);
+    }
+
+    public ObservableCollection<Document> Left { get; } = [];
+
+    public ObservableCollection<Document> Top { get; } = [];
+
+    public ObservableCollection<Document> Right { get; } = [];
+
+    public ObservableCollection<Document> Bottom { get; } = [];
+
+    public ObservableCollection<LayoutContainer> Children { get; } = [];
+}
--- src/Dock/Document.cs
+++ src/Dock/Document.cs
@@ -5,6 +5,16 @@ namespace Dock;
 
 public partial class Document : ContentControl
 {
+    public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title),
+                                                                                          typeof(string),
+                                                                                          typeof(Document),
+                                                                                          new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty CanAnchorProperty = DependencyProperty.Register(nameof(CanAnchor),
+                                                                                              typeof(bool),
+                                                                                              typeof(Document),
+                                                                                              new PropertyMetadata(true));
+
     public static readonly DependencyProperty CanCloseProperty = DependencyProperty.Register(nameof(CanClose),
                                                                                              typeof(bool),
                                                                                              typeof(Document),
@@ -13,6 +23,18 @@ public partial class Document : ContentControl
     public Document()
     {
         DefaultStyleKey = typeof(Document);
+    }
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public bool CanAnchor
+    {
+        get => (bool)GetValue(CanAnchorProperty);
+        set => SetValue(CanAnchorProperty, value);
     }
 
     public bool CanClose
