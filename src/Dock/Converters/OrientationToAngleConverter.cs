26747dceefb0631b9de03187ac311c1da760e410
王先生 <1324771795@qq.com>
2025-02-22 15:05:24 +0800

    添加 OrientationToAngleConverter 和 UI 更新
    在 `Sidebar.xaml` 中引入 `CommunityToolkit.WinUI` 和 `Dock.Converters` 命名空间，新增 `OrientationToAngleConverter` 资源，重命名 `ItemsRepeater` 为 `Items`，并添加 `VisualStateManager` 以动态调整边距。更新 `ItemsRepeater.ItemTemplate` 中的 `LayoutTransformControl`，绑定 `Orientation` 的角度。
    
    在 `OrientationToAngleConverter.cs` 中实现 `OrientationToAngleConverter` 类，返回基于 `Orientation` 的角度值，`ConvertBack` 方法抛出 `NotImplementedException`。

---
+++ src/Dock/Converters/OrientationToAngleConverter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+
+namespace Dock.Converters;
+
+public partial class OrientationToAngleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is Orientation orientation)
+        {
+            return orientation switch
+            {
+                Orientation.Horizontal => 0,
+                Orientation.Vertical => 90,
+                _ => 0
+            };
+        }
+
+        return 0;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
