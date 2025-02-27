5502a7ea4b33718fb6053f18925b9c34396a03c6
王先生 <1324771795@qq.com>
2025-02-27 15:22:03 +0800

    添加选项卡位置和宽度依赖属性
    在 `DocumentGroup.xaml.cs` 中引入 `WinUI.Dock.Enums` 命名空间，定义了 `TabPositionProperty` 和 `IsTabWidthBasedOnContentProperty` 依赖属性。新增属性访问器以支持选项卡位置和宽度设置。同时，在 `TabPosition.cs` 中定义了 `TabPosition` 枚举，包含 `Top` 和 `Bottom` 两个值。

--- src/WinUI.Dock/DocumentGroup.xaml.cs
+++ src/WinUI.Dock/DocumentGroup.xaml.cs
@@ -1,12 +1,35 @@
 ﻿using WinUI.Dock.Abstracts;
+using WinUI.Dock.Enums;
 
 namespace WinUI.Dock;
 
 public partial class DocumentGroup : DockContainer
 {
+    public static readonly DependencyProperty TabPositionProperty = DependencyProperty.Register(nameof(TabPosition),
+                                                                                                typeof(TabPosition),
+                                                                                                typeof(DocumentGroup),
+                                                                                                new PropertyMetadata(TabPosition.Top));
+
+    public static readonly DependencyProperty IsTabWidthBasedOnContentProperty = DependencyProperty.Register(nameof(IsTabWidthBasedOnContent),
+                                                                                                             typeof(bool),
+                                                                                                             typeof(DocumentGroup),
+                                                                                                             new PropertyMetadata(false));
+
     public DocumentGroup()
     {
         DefaultStyleKey = typeof(DocumentGroup);
+    }
+
+    public TabPosition TabPosition
+    {
+        get => (TabPosition)GetValue(TabPositionProperty);
+        set => SetValue(TabPositionProperty, value);
+    }
+
+    public bool IsTabWidthBasedOnContent
+    {
+        get => (bool)GetValue(IsTabWidthBasedOnContentProperty);
+        set => SetValue(IsTabWidthBasedOnContentProperty, value);
     }
 
     protected override void LoadChildren()
