199e739fa2ad7e18fb6cd63f3ed6411278a36d5e
王先生 <1324771795@qq.com>
2025-04-07 16:30:13 +0800

    重构项目。

---
+++ src/WinUI.Dock/WinUIDockResources.cs
@@ -0,0 +1,9 @@
+﻿namespace WinUI.Dock;
+
+public partial class WinUIDockResources : ResourceDictionary
+{
+    public WinUIDockResources()
+    {
+        Source = new Uri("ms-appx:///WinUI.Dock/Themes/Resources.xaml", UriKind.Absolute);
+    }
+}
